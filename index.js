d276b883d0d1e223ffadb2a5f0a4d129c8e4d10a
kiss_ivan@hotmail.com <79423340+Ivansallas@users.noreply.github.com>
2024-06-19 23:10:55 -0300

    oi

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function pegaArquivo(caminhoDoArquivo) {
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
         console.log(chalk.green(texto))
-    })
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
 }
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');

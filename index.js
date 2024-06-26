7197faf73b8d8a9956ca4711b380c4cd2a15253d
Ivan <79423340+Ivansallas@users.noreply.github.com>
2024-06-26 20:40:43 -0300

    ok

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto))
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');

c2f653909664b54bed09a0dfb932a48b51158ea9
Ivan <79423340+Ivansallas@users.noreply.github.com>
2024-07-03 19:44:56 -0300

    ok

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+// Importa os módulos fs e chalk
 import fs from 'fs';
 import chalk from 'chalk';
 
+// Função que extrai links de um texto utilizando regex
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; // Expressão regular para capturar links no formato [texto](url) 
+    const capturas = [...texto.matchAll(regex)]; // MatchAll retorna um iterador com todas as correspondências
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] })); // Mapeia as capturas para um array de objetos no formato { texto: url }
+
+    return resultados;
+}
+
+// Função para tratar erros e lançar uma mensagem de erro customizada
 function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
+// Função assíncrona para ler arquivo e extrair links
 async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
         console.log(extraiLinks(texto));
     } catch (erro) {
-        trataErro(erro)
+        trataErro(erro);
     }
 }
 
-// Chamada da função para ler um arquivo específico
+// Chama a função pegaArquivo com o caminho do arquivo a ser lido
 pegaArquivo('./arquivos/texto.md');
